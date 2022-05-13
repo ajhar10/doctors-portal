673e16f9fc09c690a71ffe896434c30ee8dd3ecb
ajhar10 <ajharrimon90@gmail.com>
2022-05-14 01:42:49 +0600

    appoinment set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appoinment from './Pages/Appoinment/Appoinment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
@@ -13,7 +14,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='/login' element={<Login />}></Route>
-        {/* <Route path='/' element={}></Route> */}
+        <Route path='/appoinment' element={<Appoinment />}></Route>
         {/* <Route path='/' element={}></Route> */}
         {/* <Route path='/' element={}></Route> */}
       </Routes>
---
+++ src/Pages/Appoinment/Appoinment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../Shared/Footer/Footer';
+
+const Appoinment = () => {
+    return (
+        <div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Appoinment;
